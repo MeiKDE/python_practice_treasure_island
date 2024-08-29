00fe83c95cd4bdf26d8fee954ffa07e9da7f86a5
Mei Zhang <rubymxzhang@gmail.com>
2024-08-29 20:09:15 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Treasure Island
+
+This is a text adventure game.      
+
+## Gameplay
+
+You are on a Treasure Island. You must find the treasure to escape the island. You can move around and interact with items to find the treasure. 
+
+## Objective 
+
+You have escaped the island and found the treasure. You have won the game!  
+
+## Credits
+
+- Game design: Mei
+
---
+++ treasure_island.py
@@ -0,0 +1,54 @@
+# This is a Python program that simulates the game "Treasure Island"
+print('''
+  ____________________________________________________________________
+ / \-----     ---------  -----------     -------------- ------    ----\
+ \_/__________________________________________________________________/
+ |~ ~~ ~~~ ~ ~ ~~~ ~ _____.----------._ ~~~  ~~~~ ~~   ~~  ~~~~~ ~~~~|
+ |  _   ~~ ~~ __,---'_       "         `. ~~~ _,--.  ~~~~ __,---.  ~~|
+ | | \___ ~~ /      ( )   "          "   `-.,' (') \~~ ~ (  / _\ \~~ |
+ |  \    \__/_   __(( _)_      (    "   "     (_\_) \___~ `-.___,'  ~|
+ |~~ \     (  )_(__)_|( ))  "   ))          "   |    "  \ ~~ ~~~ _ ~~|
+ |  ~ \__ (( _( (  ))  ) _)    ((     \\//    " |   "    \_____,' | ~|
+ |~~ ~   \  ( ))(_)(_)_)|  "    ))    //\\ " __,---._  "  "   "  /~~~|
+ |    ~~~ |(_ _)| | |   |   "  (   "      ,-'~~~ ~~~ `-.   ___  /~ ~ |
+ | ~~     |  |  |   |   _,--- ,--. _  "  (~~  ~~~~  ~~~ ) /___\ \~~ ~|
+ |  ~ ~~ /   |      _,----._,'`--'\.`-._  `._~~_~__~_,-'  |H__|  \ ~~|
+ |~~    / "     _,-' / `\ ,' / _'  \`.---.._          __        " \~ |
+ | ~~~ / /   .-' , / ' _,'_  -  _ '- _`._ `.`-._    _/- `--.   " " \~|
+ |  ~ / / _-- `---,~.-' __   --  _,---.  `-._   _,-'- / ` \ \_   " |~|
+ | ~ | | -- _    /~/  `-_- _  _,' '  \ \_`-._,-'  / --   \  - \_   / |
+ |~~ | \ -      /~~| "     ,-'_ /-  `_ ._`._`-...._____...._,--'  /~~|
+ | ~~\  \_ /   /~~/    ___  `---  ---  - - ' ,--.     ___        |~ ~|
+ |~   \      ,'~~|  " (o o)   "         " " |~~~ \_,-' ~ `.     ,'~~ |
+ | ~~ ~|__,-'~~~~~\    \"/      "  "   "    /~ ~~   O ~ ~~`-.__/~ ~~~|
+ |~~~ ~~~  ~~~~~~~~`.______________________/ ~~~    |   ~~~ ~~ ~ ~~~~|
+ |____~jrei~__~_______~~_~____~~_____~~___~_~~___~\_|_/ ~_____~___~__|
+ / \----- ----- ------------  ------- ----- -------  --------  -------\
+ \_/__________________________________________________________________/
+''')
+
+# Introduction to the game  
+print('''
+    Welcome to Treasure Island.
+    Your mission is to find the treasure.
+    ''')
+
+choice1=input('You\'re at a crossroad. Where do you want to go? Type "left" or "right": \n ').lower()
+
+if choice1=='right':
+    print('You fell into a hole. Game Over!')
+else:
+    choice2=input('Do you want to swim or wait? Type "swim" or "wait":\n ').lower()
+    if choice2=='swim':
+        print('You got attacked by a trout. Game Over!')
+    else:
+        choice3=input('which door do you want to open? Type "yellow", "blue" or "red": \n ').lower()
+        if choice3=='blue':
+            print('You\'ve been eaten by beasts. Game Over!')
+        elif choice3=='red':
+            print('Youve been burned by fire. Game Over!')
+        elif choice3=='yellow':
+            print('You found the treasure! You Win!')
+        else:
+            print('Invalid input. Game Over!')  
+
